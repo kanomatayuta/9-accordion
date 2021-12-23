a3b6a117a6fbcfffc943fbd2ce07179f6cb5b4dc
鹿又勇太 <>
2021-12-23 22:15:22 +0900

    first Submission of assignments

---
+++ accordion/js/sample.js
@@ -0,0 +1,17 @@
+// 質問タブを変数に入れる
+const tab = document.querySelectorAll(".accordion__tab-icon");
+// 回答コンテンツを変数に入れる
+const contents = document.querySelectorAll(".accordion__content");
+
+// 繰り返し(ループ)
+// item初期値が0からtab.length(.accordion__tab-icon)の数だけ繰り返す。
+for (let item = 0; item < tab.length; item++) {
+  // 質問タブをクリックしたら実行
+  console.log(item);
+  tab[item].addEventListener("click", function () {
+    // クリックした質問タブのactiveクラスを付け替える
+    this.classList.toggle("active");
+    // クリックした質問タブのindex番号の回答コンテンツのactiveクラスを付け替える
+    contents[item].classList.toggle("active");
+  });
+}
