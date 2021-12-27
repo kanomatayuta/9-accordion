50d197fdc339e1cb9f999245028c97ee35cda8ce
鹿又勇太 <>
2021-12-27 23:12:25 +0900

    correction[JS variable name change]

--- accordion/js/sample.js
+++ accordion/js/sample.js
@@ -4,13 +4,13 @@ const tab = document.querySelectorAll(".accordion__tab-icon");
 const contents = document.querySelectorAll(".accordion__content");
 
 // 繰り返し(ループ)
-// item初期値が0からtab.length(.accordion__tab-icon)の数だけ繰り返す。
-for (let item = 0; item < tab.length; item++) {
+// count初期値が0からtab.length(.accordion__tab-icon)の数だけ繰り返す。
+for (let count = 0; count < tab.length; count++) {
   // 質問タブをクリックしたら実行
-  tab[item].addEventListener("click", function () {
+  tab[count].addEventListener("click", function () {
     // クリックした質問タブのactiveクラスを付け替える
     this.classList.toggle("active");
     // クリックした質問タブのindex番号の回答コンテンツのactiveクラスを付け替える
-    contents[item].classList.toggle("active");
+    contents[count].classList.toggle("active");
   });
 }
